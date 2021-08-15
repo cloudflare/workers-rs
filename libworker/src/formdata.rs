404708042488ca904dd45bc0191a66581b17ffe5
Steve Manuel <nilslice@gmail.com>
2021-08-15 16:44:02 -0700

    feat: add support for formdata on request and libworker module

---
+++ libworker/src/formdata.rs
@@ -0,0 +1,59 @@
+use std::collections::HashMap;
+
+use crate::error::Error;
+use crate::Result;
+
+use edgeworker_ffi::FormData as EdgeFormData;
+
+use wasm_bindgen::prelude::*;
+
+#[derive(Debug)]
+pub struct FormData(EdgeFormData);
+
+impl FormData {
+    pub fn new() -> Self {
+        Self(EdgeFormData::new().unwrap())
+    }
+
+    pub fn get(&self, name: &str) -> Option<String> {
+        let val = self.0.get(name);
+        if val.is_undefined() {
+            return None;
+        }
+
+        val.as_string()
+    }
+
+    pub fn has(&self, name: &str) -> bool {
+        self.0.has(name)
+    }
+
+    pub fn append(&mut self, name: &str, value: &str) -> Result<()> {
+        self.0.append_with_str(name, value).map_err(Error::from)
+    }
+
+    pub fn set(&mut self, name: &str, value: &str) -> Result<()> {
+        self.0.set_with_str(name, value).map_err(Error::from)
+    }
+
+    pub fn delete(&mut self, name: &str) {
+        self.0.delete(name)
+    }
+}
+
+impl From<JsValue> for FormData {
+    fn from(val: JsValue) -> Self {
+        FormData(val.into())
+    }
+}
+
+impl From<HashMap<&dyn AsRef<&str>, &dyn AsRef<&str>>> for FormData {
+    fn from(m: HashMap<&dyn AsRef<&str>, &dyn AsRef<&str>>) -> Self {
+        let mut formdata = FormData::new();
+        for (k, v) in m {
+            // TODO: determine error case and consider how to handle
+            formdata.set(k.as_ref(), v.as_ref()).unwrap();
+        }
+        formdata
+    }
+}
--- libworker/src/lib.rs
+++ libworker/src/lib.rs
@@ -3,6 +3,7 @@ mod date;
 pub mod durable;
 mod env;
 mod error;
+mod formdata;
 mod global;
 mod headers;
 mod http;
@@ -16,6 +17,7 @@ pub type Result<T> = StdResult<T, error::Error>;
 
 pub use crate::date::{Date, DateInit};
 pub use crate::env::Env;
+pub use crate::formdata::FormData;
 pub use crate::global::Fetch;
 pub use crate::headers::Headers;
 pub use crate::request::Request;
@@ -30,6 +32,7 @@ pub mod prelude {
     pub use crate::cf::Cf;
     pub use crate::date::{Date, DateInit};
     pub use crate::env::Env;
+    pub use crate::formdata::FormData;
     pub use crate::global::Fetch;
     pub use crate::headers::Headers;
     pub use crate::http::Method;
