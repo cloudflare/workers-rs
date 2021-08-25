550ffd80526cc6bda5854884dc4d351efa3b73df
Cass Fridkin <cass@cloudflare.com>
2021-08-25 12:24:19 -0500

    feat(build): make building rust-workers cross-platform
    add the rust-worker-build binary which wraps wasm-bindgen
    and makes the necessary changes for our workers to be working.
    
    there's no cargo install step yet because we haven't published it,
    so for now just cargo install --path

--- rust-sandbox/worker/export_wasm.mjs
+++
@@ -1,7 +0,0 @@
-import * as index_bg from "./index_bg.mjs";
-import _wasm from "./index_bg.wasm";
-
-const _wasm_memory = new WebAssembly.Memory({initial: 512});
-let importsObject = {env: {memory: _wasm_memory}, "./index_bg.js": index_bg};
-
-export default new WebAssembly.Instance(_wasm, importsObject).exports;
