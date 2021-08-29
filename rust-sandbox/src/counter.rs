55e234044d20ca58c050bb87f9665937c5a85ccb
Steve Manuel <smanuel@cloudflare.com>
2021-08-29 03:06:29 -0600

    chore: show use of env in durable object fetch

--- rust-sandbox/src/counter.rs
+++ rust-sandbox/src/counter.rs
@@ -5,20 +5,21 @@ pub struct Counter {
     count: usize,
     state: State,
     initialized: bool,
+    env: std::sync::Arc<Env>,
 }
 
 #[durable_object]
 impl DurableObject for Counter {
-    fn new(state: worker::durable::State, _env: worker::Env) -> Self {
+    fn new(state: worker::durable::State, env: worker::Env) -> Self {
         Self {
             count: 0,
             initialized: false,
             state,
+            env: std::sync::Arc::new(env),
         }
     }
 
     async fn fetch(&mut self, _req: worker::Request) -> worker::Result<worker::Response> {
-        // Get info from last backup
         if !self.initialized {
             self.initialized = true;
             self.count = self.state.storage().get("count").await.unwrap_or(0);
@@ -26,6 +27,10 @@ impl DurableObject for Counter {
 
         self.count += 10;
         self.state.storage().put("count", self.count).await?;
-        Response::ok(self.count.to_string())
+        Response::ok(&format!(
+            "count: {}, secret: {}",
+            self.count.to_string(),
+            &self.env.secret("SOME_SECRET")?.to_string()
+        ))
     }
 }
