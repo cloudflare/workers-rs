202fa55d8d9c73599735446ad7a9b40cf4d9b25b
Steve Manuel <smanuel@cloudflare.com>
2021-08-30 17:07:55 -0600

    chore: re-export types from worker crate

--- worker/src/lib.rs
+++ worker/src/lib.rs
@@ -1,9 +1,11 @@
 pub use libworker::durable;
+pub use libworker::durable::*;
 pub use libworker::prelude::*;
 pub use macros::{durable_object, event};
 
 pub use async_trait;
 pub use edgeworker_sys;
 pub use js_sys;
+pub use libworker;
 pub use wasm_bindgen;
 pub use wasm_bindgen_futures;
