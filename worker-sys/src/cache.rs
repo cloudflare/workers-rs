f33a8fb44608e0dc15b866825d3238926244d40a
Cass Fridkin <cass@cloudflare.com>
2021-11-19 14:29:29 -0600

    feat(cache): accept strings as urls + add docs
    The js api accepts strings and converts them into the URL for new
    GET requests. By introducing the `CacheKey` type we can replicate
    this behavior in rust.
    
    Also, added some docs just ripped from https://developers.cloudflare.com/workers/runtime-apis/cache
    and slightly modified for rust.

--- worker-sys/src/cache.rs
+++ worker-sys/src/cache.rs
@@ -9,13 +9,22 @@ extern "C" {
     pub type Cache;
 
     #[wasm_bindgen(structural, method, js_class=Cache, js_name = put)]
-    pub fn put(this: &Cache, request: Request, response: Response) -> ::js_sys::Promise;
+    pub fn put_request(this: &Cache, request: Request, response: Response) -> ::js_sys::Promise;
+
+    #[wasm_bindgen(structural, method, js_class=Cache, js_name = put)]
+    pub fn put_url(this: &Cache, url: String, response: Response) -> ::js_sys::Promise;
 
     #[wasm_bindgen(structural, method, js_class=Cache, js_name=match)]
-    pub fn r#match(this: &Cache, request: Request, options: JsValue) -> ::js_sys::Promise;
+    pub fn match_request(this: &Cache, request: Request, options: JsValue) -> ::js_sys::Promise;
+
+    #[wasm_bindgen(structural, method, js_class=Cache, js_name=match)]
+    pub fn match_url(this: &Cache, url: String, options: JsValue) -> ::js_sys::Promise;
 
     #[wasm_bindgen(structural, method, js_class=Cache, js_name=delete)]
-    pub fn delete(this: &Cache, request: Request, options: JsValue) -> ::js_sys::Promise;
+    pub fn delete_request(this: &Cache, request: Request, options: JsValue) -> ::js_sys::Promise;
+
+    #[wasm_bindgen(structural, method, js_class=Cache, js_name=delete)]
+    pub fn delete_url(this: &Cache, request: String, options: JsValue) -> ::js_sys::Promise;
 }
 
 // `caches` global object
@@ -25,10 +34,10 @@ extern "C" {
     #[wasm_bindgen(js_name = Caches)]
     pub type Caches;
 
-    #[wasm_bindgen(method, structural, getter, js_class = "Caches", js_name = default)]
-    pub fn get_default_cache(this: &Caches) -> Cache;
+    #[wasm_bindgen(method, structural, getter, js_class = "Caches")]
+    pub fn default(this: &Caches) -> Cache;
 
-    #[wasm_bindgen(method, structural, js_class = "Caches", js_name = open)]
-    pub fn get_cache_from_name(this: &Caches, cache_name: String) -> ::js_sys::Promise;
+    #[wasm_bindgen(method, structural, js_class = "Caches")]
+    pub fn open(this: &Caches, cache_name: String) -> ::js_sys::Promise;
 
 }
--- worker/src/cache.rs
+++ worker/src/cache.rs
@@ -9,6 +9,28 @@ use crate::request::Request;
 use crate::response::Response;
 use crate::Result;
 
+/// Provides access to the [cache api](https://developers.cloudflare.com/workers/runtime-apis/cache).
+///
+/// Our implementation of the Cache API respects the following HTTP headers on the response passed to `put()`:
+///
+/// - `Cache-Control`
+///   - Controls caching directives.
+///     This is consistent with [Cloudflare Cache-Control Directives](https://developers.cloudflare.com/cache/about/cache-control#cache-control-directives).
+///     Refer to [Edge TTL](https://developers.cloudflare.com/cache/how-to/configure-cache-status-code#edge-ttl) for a list of HTTP response codes and their TTL when Cache-Control directives are not present.
+/// - `Cache-Tag`
+///   - Allows resource purging by tag(s) later (Enterprise only).
+/// - `ETag`
+///   - Allows `cache.match()` to evaluate conditional requests with If-None-Match.
+/// - `Expires`
+///   - A string that specifies when the resource becomes invalid.
+/// - `Last-Modified`
+///   - Allows `cache.match()` to evaluate conditional requests with If-Modified-Since.
+///
+/// This differs from the web browser Cache API as they do not honor any headers on the request or response.
+///
+/// Responses with `Set-Cookie` headers are never cached, because this sometimes indicates that the response contains unique data. To store a response with a `Set-Cookie` header, either delete that header or set `Cache-Control: private=Set-Cookie` on the response before calling `cache.put()`.
+///
+/// Use the `Cache-Control` method to store the response without the `Set-Cookie` header.
 pub struct Cache {
     inner: EdgeCache,
 }
@@ -18,15 +40,16 @@ impl Default for Cache {
         let global: WorkerGlobalScope = js_sys::global().unchecked_into();
 
         Self {
-            inner: global.caches().get_default_cache(),
+            inner: global.caches().default(),
         }
     }
 }
 
 impl Cache {
+    /// Opens a [`Cache`] by name. To access the default global cache, use [`Cache::default()`](`Default::default`).
     pub async fn open(name: String) -> Self {
         let global: WorkerGlobalScope = js_sys::global().unchecked_into();
-        let cache = global.caches().get_cache_from_name(name);
+        let cache = global.caches().open(name);
 
         // unwrap is safe because this promise never rejects
         // https://developer.mozilla.org/en-US/docs/Web/API/CacheStorage/open
@@ -35,38 +58,124 @@ impl Cache {
         Self { inner }
     }
 
-    pub async fn put(&self, request: &Request, response: Response) -> Result<()> {
-        // TODO take the request by value and get the inner via From. for some reason that isn't working so...here we are
-        // i don't think it's actually too expensive? like it's just cloning a pointer i think.
-        let ffi_request = request.inner().clone()?;
-        let ffi_response = response.into();
-        let result = self.inner.put(ffi_request, ffi_response);
-        let _ = JsFuture::from(result).await?;
+    /// Adds to the cache a [`Response`] keyed to the given request.
+    ///
+    /// The `stale-while-revalidate` and `stale-if-error` directives are not supported
+    /// when using the `cache.put` or `cache.match` methods.
+    ///
+    /// Will throw an error if:
+    /// - the request passed is a method other than GET.
+    /// - the response passed has a status of 206 Partial Content.
+    /// - the response passed contains the header `Vary: *` (required by the Cache API specification).
+    pub async fn put<K: Into<CacheKey>>(&self, key: K, response: Response) -> Result<()> {
+        let promise = match key.into() {
+            CacheKey::Url(url) => self.inner.put_url(url, response.into()),
+            CacheKey::Request(request) => {
+                // TODO take the request by value and get the inner via From. for some reason that isn't working so...here we are
+                // i don't think it's actually too expensive? like it's just cloning a pointer i think.
+                let ffi_request = request.inner().clone()?;
+                self.inner.put_request(ffi_request, response.into())
+            }
+        };
+        let _ = JsFuture::from(promise).await?;
         Ok(())
     }
 
-    pub async fn r#match(&self, request: &Request, ignore_method: bool) -> Result<Response> {
+    /// Returns the [`Response`] object keyed to that request. Never sends a subrequest to the origin. If no matching response is found in cache, returns `None`.
+    ///
+    /// Unlike the browser Cache API, Cloudflare Workers do not support the `ignoreSearch` or `ignoreVary` options on `match()`. You can accomplish this behavior by removing query strings or HTTP headers at `put()` time.
+    ///
+    /// Our implementation of the Cache API respects the following HTTP headers on the request passed to `match()`:
+    ///
+    /// - Range
+    ///   - Results in a `206` response if a matching response with a Content-Length header is found. Your Cloudflare cache always respects range requests, even if an `Accept-Ranges` header is on the response.
+    /// - If-Modified-Since
+    ///   - Results in a `304` response if a matching response is found with a `Last-Modified` header with a value after the time specified in `If-Modified-Since`.
+    /// - If-None-Match
+    ///   - Results in a `304` response if a matching response is found with an `ETag` header with a value that matches a value in `If-None-Match.`
+    pub async fn r#match<K: Into<CacheKey>>(
+        &self,
+        key: K,
+        ignore_method: bool,
+    ) -> Result<Option<Response>> {
         let options = JsValue::from_serde(&MatchOptions { ignore_method })?;
-        let ffi_request = request.inner().clone()?;
-        let result = self.inner.r#match(ffi_request, options);
-        let edge_response: EdgeResponse = JsFuture::from(result).await?.into();
-        let response = Response::from(edge_response);
+        let promise = match key.into() {
+            CacheKey::Url(url) => self.inner.match_url(url, options),
+            CacheKey::Request(request) => {
+                // TODO: the same thing as above, why can't i use Into::into()?
+                let ffi_request = request.inner().clone()?;
+                self.inner.match_request(ffi_request, options)
+            }
+        };
 
-        Ok(response)
+        // `match` returns either a response or undefined
+        let result = JsFuture::from(promise).await?;
+        if result.is_undefined() {
+            Ok(None)
+        } else {
+            let edge_response: EdgeResponse = result.into();
+            let response = Response::from(edge_response);
+            Ok(Some(response))
+        }
     }
 
-    pub async fn delete(&self, request: &Request, ignore_method: bool) -> Result<bool> {
+    pub async fn delete<K: Into<CacheKey>>(
+        &self,
+        key: K,
+        ignore_method: bool,
+    ) -> Result<CacheDeletionOutcome> {
         let options = JsValue::from_serde(&MatchOptions { ignore_method })?;
-        let ffi_request = request.inner().clone()?;
-        let result = JsFuture::from(self.inner.delete(ffi_request, options)).await?;
+
+        let promise = match key.into() {
+            CacheKey::Url(url) => self.inner.delete_url(url, options),
+            CacheKey::Request(request) => {
+                // TODO: the same thing as above, why can't i use Into::into()?
+                let ffi_request = request.inner().clone()?;
+                self.inner.delete_request(ffi_request, options)
+            }
+        };
+        let result = JsFuture::from(promise).await?;
+
         // unwrap is safe because we know this is a boolean
         // https://developers.cloudflare.com/workers/runtime-apis/cache#delete
-        Ok(result.as_bool().unwrap())
+        if result.as_bool().unwrap() {
+            Ok(CacheDeletionOutcome::Success)
+        } else {
+            Ok(CacheDeletionOutcome::ResponseNotFound)
+        }
     }
 }
 
+/// Can contain one possible property: `ignoreMethod`
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
 struct MatchOptions {
+    /// Consider the request method a `GET` regardless of its actual value.
     ignore_method: bool,
+}
+
+/// The `String` or `Request` object used as the lookup key. `String`s are interpreted as the URL for a new `Request` object.
+pub enum CacheKey {
+    Url(String),
+    Request(Request),
+}
+
+impl<S: Into<String>> From<S> for CacheKey {
+    fn from(url: S) -> Self {
+        Self::Url(url.into())
+    }
+}
+
+impl From<Request> for CacheKey {
+    fn from(request: Request) -> Self {
+        Self::Request(request)
+    }
+}
+
+/// Successful outcomes when attempting to delete a `Response` from the cache
+pub enum CacheDeletionOutcome {
+    /// The response was cached but is now deleted
+    Success,
+    /// The response was not in the cache at the time of deletion.
+    ResponseNotFound,
 }
