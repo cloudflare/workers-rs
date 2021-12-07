e4278224063052f2278633c1c305467c20d37646
Steve Manuel <smanuel@cloudflare.com>
2021-12-07 13:50:26 -0700

    chore: update url in DO fetch, and add comment about change

--- worker-sandbox/src/lib.rs
+++ worker-sandbox/src/lib.rs
@@ -278,7 +278,10 @@ pub async fn main(req: Request, env: Env) -> Result<Response> {
         .get_async("/durable/:id", |_req, ctx| async move {
             let namespace = ctx.durable_object("COUNTER")?;
             let stub = namespace.id_from_name("A")?.get_stub()?;
-            stub.fetch_with_str("/").await
+            // when calling fetch to a Durable Object, a full URL must be used. Alternatively, a
+            // compatibility flag can be provided in wrangler.toml to opt-in to older behavior:
+            // https://developers.cloudflare.com/workers/platform/compatibility-dates#durable-object-stubfetch-requires-a-full-url
+            stub.fetch_with_str("https://fake-host/").await
         })
         .get("/secret", |_req, ctx| {
             Response::ok(ctx.secret("SOME_SECRET")?.to_string())
