45505b30ad951a541c342f7457ef6adefc23f1b9
Zeb Piasecki <zeb@cloudflare.com>
2022-01-21 09:28:51 -0500

    feat: begin adding rust integration tests

--- worker-sandbox/test.sh
+++ worker-sandbox/test.sh
@@ -17,19 +17,4 @@ while ! nc -z localhost 8787; do
   sleep 0.1
 done
 
-echo "\nWrangler started, running tests via curl...\n"
-
-checkRoute() {
-    echo "Requesting $1 /$2"
-    curl -f -s "http://127.0.0.1:8787/$2" &> /dev/null
-    echo "Done"
-}
-
-checkRoute GET request
-checkRoute GET async-request
-checkRoute GET test-data
-checkRoute GET 
-checkRoute GET request
-
-checkRoute GET user/example/test
-checkRoute GET user/example
+# TODO
