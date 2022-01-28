62e9b24e5a7c1d46bac34cf8127c1550c411a1d4
ByteAlex <bytealex@zerotwo.bot>
2022-01-28 20:31:41 +0100

    Cleanup + Docs

--- worker-sys/src/websocket.rs
+++ worker-sys/src/websocket.rs
@@ -5,12 +5,13 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern "C" {
-    #[wasm_bindgen (extends = :: js_sys :: Object , js_name = WebSocketPair)]
+
+    #[wasm_bindgen(extends = js_sys::Object, js_name = WebSocketPair)]
     #[derive(Debug, Clone, PartialEq, Eq)]
     #[doc = "The `WebSocketPair` dictionary."]
     pub type WebSocketPair;
 
-    #[wasm_bindgen(constructor, js_class=WebSocketPair)]
+    #[wasm_bindgen(constructor, js_class = WebSocketPair)]
     pub fn new() -> WebSocketPair;
 }
 
@@ -28,28 +29,33 @@ impl WebSocketPair {
 
 #[wasm_bindgen]
 extern "C" {
-    # [wasm_bindgen (extends = EventTarget , extends = :: js_sys :: Object , js_name = WebSocket , typescript_type = "WebSocket")]
+
+    #[wasm_bindgen(extends = EventTarget, extends = js_sys::Object, js_name = WebSocket)]
     #[derive(Debug, Clone, PartialEq, Eq)]
     #[doc = "The `WebSocket` class."]
     #[doc = ""]
     #[doc = "[MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/API/WebSocket)"]
     pub type WebSocket;
-    # [wasm_bindgen (catch, structural, method, js_class = "WebSocket", js_name = accept)]
+
+    #[wasm_bindgen(catch, structural, method, js_class = "WebSocket", js_name = accept)]
     #[doc = "Accepts the server side of the WebSocket."]
     #[doc = ""]
     #[doc = "[CF Documentation](https://developers.cloudflare.com/workers/runtime-apis/websockets#accept)"]
     pub fn accept(this: &WebSocket) -> Result<(), JsValue>;
-    # [wasm_bindgen (catch , method , structural , js_class = "WebSocket" , js_name = close)]
+
+    #[wasm_bindgen(catch, method, structural, js_class = "WebSocket", js_name = close)]
     #[doc = "The `close()` method."]
     #[doc = ""]
     #[doc = "[MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/API/WebSocket/close)"]
     pub fn close(this: &WebSocket) -> Result<(), JsValue>;
-    # [wasm_bindgen (catch , method , structural , js_class = "WebSocket" , js_name = close)]
+
+    #[wasm_bindgen(catch, method, structural, js_class = "WebSocket", js_name = close)]
     #[doc = "The `close()` method."]
     #[doc = ""]
     #[doc = "[MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/API/WebSocket/close)"]
     pub fn close_with_code(this: &WebSocket, code: u16) -> Result<(), JsValue>;
-    # [wasm_bindgen (catch , method , structural , js_class = "WebSocket" , js_name = close)]
+
+    #[wasm_bindgen(catch, method, structural, js_class = "WebSocket", js_name = close)]
     #[doc = "The `close()` method."]
     #[doc = ""]
     #[doc = "[MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/API/WebSocket/close)"]
@@ -58,20 +64,23 @@ extern "C" {
         code: u16,
         reason: &str,
     ) -> Result<(), JsValue>;
-    # [wasm_bindgen (catch , method , structural , js_class = "WebSocket" , js_name = send)]
+
+    #[wasm_bindgen(catch, method, structural, js_class = "WebSocket", js_name = send)]
     #[doc = "The `send()` method."]
     #[doc = ""]
     #[doc = "[MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/API/WebSocket/send)"]
     pub fn send_with_str(this: &WebSocket, data: &str) -> Result<(), JsValue>;
-    # [wasm_bindgen (catch , method , structural , js_class = "WebSocket" , js_name = send)]
+
+    #[wasm_bindgen(catch, method, structural, js_class = "WebSocket", js_name = send)]
     #[doc = "The `send()` method."]
     #[doc = ""]
     #[doc = "[MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/API/WebSocket/send)"]
     pub fn send_with_u8_array(this: &WebSocket, data: Uint8Array) -> Result<(), JsValue>;
-    # [wasm_bindgen (catch , method , structural , js_class = "WebSocket" , js_name = addEventListener)]
-    #[doc = "The `send()` method."]
+
+    #[wasm_bindgen(catch, method, structural, js_class = "WebSocket", js_name = addEventListener)]
+    #[doc = "The `addEventListener()` method."]
     #[doc = ""]
-    #[doc = "[MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/API/WebSocket/send)"]
+    #[doc = "[CF Documentation](https://developers.cloudflare.com/workers/runtime-apis/websockets#addeventlistener)"]
     pub fn add_event_listener(
         this: &WebSocket,
         r#type: JsValue,
--- worker/src/websocket.rs
+++ worker/src/websocket.rs
@@ -8,6 +8,7 @@ use wasm_bindgen::{JsCast, JsValue};
 use wasm_bindgen_futures::JsFuture;
 use worker_sys::Response;
 
+/// Struct holding the values for a JavaScript `WebSocketPair`
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct WebSocketPair {
     pub client: WebSocket,
@@ -15,6 +16,7 @@ pub struct WebSocketPair {
 }
 
 impl WebSocketPair {
+    /// Creates a new `WebSocketPair`.
     pub fn new() -> Result<Self> {
         let mut pair = worker_sys::WebSocketPair::new();
         let client = pair.client()?.into();
@@ -23,27 +25,32 @@ impl WebSocketPair {
     }
 }
 
+/// Wrapper struct for underlying worker-sys `WebSocket`
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct WebSocket {
     socket: worker_sys::WebSocket,
 }
 
 impl WebSocket {
+    /// Accept the server-side of a websocket connection
     pub fn accept(&self) -> Result<()> {
         self.socket.accept().map_err(|err| Error::from(err))
     }
 
+    /// Serialize data into a string using serde and send it through the `WebSocket`
     pub fn send<T: Serialize>(&self, data: &T) -> Result<()> {
         let value = serde_json::to_string(data)?;
         self.send_with_str(value.as_str())
     }
 
+    /// Sends a raw string through the `WebSocket`
     pub fn send_with_str<S: AsRef<str>>(&self, data: S) -> Result<()> {
         self.socket
             .send_with_str(data.as_ref())
             .map_err(|err| Error::from(err))
     }
 
+    /// Sends raw binary data through the `WebSocket`.
     pub fn send_with_binary(&self, bytes: Vec<u8>) -> Result<()> {
         let array = js_sys::Uint8Array::new_with_length(bytes.len() as u32);
         array.copy_from(&bytes);
@@ -52,6 +59,16 @@ impl WebSocket {
             .map_err(|err| Error::from(err))
     }
 
+    /// Closes this channel.
+    /// This method translates to three different underlying method calls based of the
+    /// parameters passed.
+    ///
+    /// If the following parameters are Some:
+    /// * `code` and `reason` -> `close_with_code_and_reason`
+    /// * `code`              -> `close_with_code`
+    /// * `reason` or `none`  -> `close`
+    ///
+    /// Effectively, if only `reason` is `Some`, the `reason` argument will be ignored.
     pub fn close<S: AsRef<str>>(&self, code: Option<u16>, reason: Option<S>) -> Result<()> {
         if let Some((code, reason)) = code.zip(reason) {
             self.socket
@@ -64,12 +81,22 @@ impl WebSocket {
         .map_err(|err| Error::from(err))
     }
 
+    /// Registers an event-handler for the `message` event.
+    ///
+    /// This event will be fired when the `WebSocket` receives a payload.
+    ///
+    /// https://developers.cloudflare.com/workers/runtime-apis/websockets#events     
     pub fn on_message<F: Fn(MessageEvent) + 'static>(&self, fun: F) -> Result<()> {
-        self.add_event_listener("message", move |event: worker_sys::MessageEvent| {
+        self.add_event_handler("message", move |event: worker_sys::MessageEvent| {
             fun(event.into());
         })
     }
 
+    /// Registers an async event-handler for the `message` event.
+    ///
+    /// This event will be fired when the `WebSocket` receives a payload.
+    ///
+    /// https://developers.cloudflare.com/workers/runtime-apis/websockets#events    
     pub fn on_message_async<
         Fut: Future<Output = ()> + 'static,
         F: Fn(MessageEvent) -> Fut + 'static,
@@ -77,18 +104,28 @@ impl WebSocket {
         &self,
         fun: F,
     ) -> Result<()> {
-        self.add_event_listener("message", move |event: worker_sys::MessageEvent| {
+        self.add_event_handler("message", move |event: worker_sys::MessageEvent| {
             let fut = fun(event.into());
             wasm_bindgen_futures::spawn_local(async move { fut.await });
         })
     }
 
+    /// Registers an event-handler for the `close` event.
+    ///
+    /// This event will be fired when the `WebSocket` closes.
+    ///
+    /// https://developers.cloudflare.com/workers/runtime-apis/websockets#events    
     pub fn on_close<F: Fn(CloseEvent) + 'static>(&self, fun: F) -> Result<()> {
-        self.add_event_listener("close", move |event: worker_sys::CloseEvent| {
+        self.add_event_handler("close", move |event: worker_sys::CloseEvent| {
             fun(event.into());
         })
     }
 
+    /// Registers an async event-handler for the `close` event.
+    ///
+    /// This event will be fired when the `WebSocket` closes.
+    ///
+    /// https://developers.cloudflare.com/workers/runtime-apis/websockets#events
     pub fn on_close_async<
         Fut: Future<Output = ()> + 'static,
         F: Fn(CloseEvent) -> Fut + 'static,
@@ -96,18 +133,28 @@ impl WebSocket {
         &self,
         fun: F,
     ) -> Result<()> {
-        self.add_event_listener("close", move |event: worker_sys::CloseEvent| {
+        self.add_event_handler("close", move |event: worker_sys::CloseEvent| {
             let fut = fun(event.into());
             wasm_bindgen_futures::spawn_local(async move { fut.await });
         })
     }
 
+    /// Registers an event-handler for the `error` event.
+    ///
+    /// This event will be fired when the `WebSocket` caught an error.
+    ///
+    /// https://developers.cloudflare.com/workers/runtime-apis/websockets#events
     pub fn on_error<F: Fn(ErrorEvent) + 'static>(&self, fun: F) -> Result<()> {
-        self.add_event_listener("error", move |event: worker_sys::ErrorEvent| {
+        self.add_event_handler("error", move |event: worker_sys::ErrorEvent| {
             fun(event.into());
         })
     }
 
+    /// Registers an async event-handler for the `error` event.
+    ///
+    /// This event will be fired when the `WebSocket` caught an error.
+    ///
+    /// https://developers.cloudflare.com/workers/runtime-apis/websockets#events
     pub fn on_error_async<
         Fut: Future<Output = ()> + 'static,
         F: Fn(ErrorEvent) -> Fut + 'static,
@@ -115,13 +162,19 @@ impl WebSocket {
         &self,
         fun: F,
     ) -> Result<()> {
-        self.add_event_listener("error", move |event: worker_sys::ErrorEvent| {
+        self.add_event_handler("error", move |event: worker_sys::ErrorEvent| {
             let fut = fun(event.into());
             wasm_bindgen_futures::spawn_local(async move { fut.await });
         })
     }
 
-    fn add_event_listener<T: FromWasmAbi + 'static, F: FnMut(T) + 'static>(
+    /// Internal utility method to avoid verbose code.
+    /// This method registers a closure in the underlying JS environment, which calls back
+    /// into the Rust/wasm environment.
+    /// Since this is a 'long living closure', we need to actively "leak" memory in this,
+    /// as we need to drop the reference to the `WasmClosure` or it will be destroyed with
+    /// it's lifetime at the end of the function.
+    fn add_event_handler<T: FromWasmAbi + 'static, F: FnMut(T) + 'static>(
         &self,
         r#type: &str,
         fun: F,
@@ -155,6 +208,7 @@ impl AsRef<worker_sys::WebSocket> for WebSocket {
 pub mod ws_events {
     use wasm_bindgen::JsValue;
 
+    /// Wrapper/Utility struct for the `web_sys::MessageEvent`
     #[derive(Debug, Clone, PartialEq, Eq)]
     pub struct MessageEvent {
         event: worker_sys::MessageEvent,
@@ -173,11 +227,13 @@ pub mod ws_events {
     }
 
     impl MessageEvent {
+        /// Gets the data/payload from the message.
         pub fn get_data(&self) -> JsValue {
             self.event.data()
         }
     }
 
+    /// Wrapper/Utility struct for the `web_sys::CloseEvent`
     #[derive(Debug, Clone, PartialEq, Eq)]
     pub struct CloseEvent {
         event: worker_sys::CloseEvent,
@@ -195,6 +251,7 @@ pub mod ws_events {
         }
     }
 
+    /// Wrapper/Utility struct for the `web_sys::ErrorEvent`
     #[derive(Debug, Clone, PartialEq, Eq)]
     pub struct ErrorEvent {
         event: worker_sys::ErrorEvent,
