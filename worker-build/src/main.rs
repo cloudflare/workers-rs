b2e9004f60890db48b1be38df79c2b4ac2e168d0
Zeb Piasecki <zeb@cloudflare.com>
2022-02-04 13:07:38 -0500

    fix: uninitialized wasm imports
    Previously we generated a "export_wasm.mjs" that imported all of the
    glue generated by wasm bindgen and modified the glue to import from the
    generated file. This caused a circular import leading to some functions,
    specifically ones that were just const aliases of builtins, generated by
    wasm-bindgen to be uninitialized and cause a crash.
    
    To fix this we move all of the code in the "export_wasm.mjs" to that
    glue file.

--- worker-build/src/main.rs
+++ worker-build/src/main.rs
@@ -107,28 +107,6 @@ fn write_worker_shims_to_worker_dir() -> Result<()> {
     // _bg implies "bindgen" from wasm-bindgen tooling
     let bg_name = format!("{}_bg", OUT_NAME);
 
-    // this shim exports a webassembly instance with 512 pages
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/WebAssembly/Memory/Memory#parameters
-    let export_wasm_content = format!(
-        r#"
-import * as {0} from "./{0}.mjs";
-import _wasm from "./{0}.wasm";
-
-const _wasm_memory = new WebAssembly.Memory({{initial: 512}});
-let importsObject = {{
-    env: {{ memory: _wasm_memory }},
-    "./{0}.js": {0}
-}};
-
-export default new WebAssembly.Instance(_wasm, importsObject).exports;
-
-"#,
-        bg_name
-    );
-    let export_wasm_path = PathBuf::from(OUT_DIR)
-        .join(WORKER_SUBDIR)
-        .join("export_wasm.mjs");
-
     // this shim just re-exports things in the pattern workers expects
     let shim_content = format!(
         r#"
@@ -146,26 +124,53 @@ export default {{ fetch, scheduled }};
     let shim_path = PathBuf::from(OUT_DIR).join(WORKER_SUBDIR).join("shim.mjs");
 
     // write our content out to files
-    for (content, path) in [
-        (export_wasm_content, export_wasm_path),
-        (shim_content, shim_path),
-    ] {
-        let mut file = File::create(path)?;
-        file.write_all(content.as_bytes())?;
-    }
+    let mut file = File::create(shim_path)?;
+    file.write_all(shim_content.as_bytes())?;
 
     Ok(())
 }
 
 fn replace_generated_import_with_custom_impl() -> Result<()> {
+    let bg_name = format!("{}_bg", OUT_NAME);
     let bindgen_glue_path = PathBuf::from(OUT_DIR)
         .join(WORKER_SUBDIR)
         .join(format!("{}_bg.mjs", OUT_NAME));
     let old_bindgen_glue = read_file_to_string(&bindgen_glue_path)?;
     let old_import = format!("import * as wasm from './{}_bg.wasm'", OUT_NAME);
-    let fixed_bindgen_glue =
-        old_bindgen_glue.replace(&old_import, "import wasm from './export_wasm.mjs'");
+    let mut fixed_bindgen_glue = old_bindgen_glue.replace(&old_import, "let wasm;");
+
+    // Previously we just used a namespace import as the wasm's import obj but this had a problem
+    // where wasm-bindgen would re-export built-in functions as a const variable. This caused a
+    // problem because the OUTNAME_bg.mjs imported the wasm from another file causing a circular
+    // import so the const function items were never initialized.
+    //
+    // Because can't use import OUTNAME_bg.mjs module and use that module's exports as the import
+    // object, we'll have to find all the exports and use that to construct the wasm's import obj.
+    let names_for_import_obj = find_exports(&fixed_bindgen_glue);
+
+    // this shim exports a webassembly instance with 512 pages
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/WebAssembly/Memory/Memory#parameters
+    let initialization_glue = format!(
+        r#"
+import _wasm from "./{0}.wasm";
+
+const _wasm_memory = new WebAssembly.Memory({{initial: 512}});
+let importsObject = {{
+    env: {{ memory: _wasm_memory }},
+    "./{0}.js": {{ {1} }}
+}};
+
+wasm = new WebAssembly.Instance(_wasm, importsObject).exports;
+"#,
+        bg_name,
+        names_for_import_obj.join(", ")
+    );
+
+    // Add the code that initializes the `wasm` variable we declared at the top of the file.
+    fixed_bindgen_glue.push_str(&initialization_glue);
+
     write_string_to_file(bindgen_glue_path, fixed_bindgen_glue)?;
+
     Ok(())
 }
 
@@ -182,4 +187,26 @@ fn write_string_to_file<P: AsRef<Path>>(path: P, contents: String) -> Result<()>
     file.write_all(contents.as_bytes())?;
 
     Ok(())
+}
+
+// TODO: Potentially rewrite this to run an actual JavaScript parser.
+fn find_exports(js: &str) -> Vec<&str> {
+    const FUNCTION_EXPORT_PREFIXES: [&str; 2] = ["export function ", "export const "];
+
+    js.lines()
+        .filter_map(|line| {
+            for prefix in FUNCTION_EXPORT_PREFIXES {
+                if line.starts_with(prefix) {
+                    let name_end_index = line
+                        .chars()
+                        .skip(prefix.len())
+                        .take_while(|c| *c != '(' && *c != ' ')
+                        .count();
+                    return Some(&line[prefix.len()..name_end_index + prefix.len()]);
+                }
+            }
+
+            None
+        })
+        .collect::<Vec<_>>()
 }
