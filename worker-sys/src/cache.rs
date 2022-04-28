fd72b3af79e294f0858d344644034499b64461d0
Frank <mengqi@cloudflare.com>
2022-04-28 15:33:07 -0700

    feat: Update cache api to use references

--- worker-sys/src/cache.rs
+++ worker-sys/src/cache.rs
@@ -5,27 +5,27 @@ use crate::{Request, Response};
 // An instance of cache
 #[wasm_bindgen]
 extern "C" {
-    #[wasm_bindgen(js_name = Cache)]
+    #[wasm_bindgen(extends = ::js_sys::Object, js_name = Cache)]
     #[derive(Debug, Clone, PartialEq)]
     pub type Cache;
 
     #[wasm_bindgen(structural, method, js_class=Cache, js_name = put)]
-    pub fn put_request(this: &Cache, request: Request, response: Response) -> ::js_sys::Promise;
+    pub fn put_request(this: &Cache, request: &Request, response: &Response) -> ::js_sys::Promise;
 
     #[wasm_bindgen(structural, method, js_class=Cache, js_name = put)]
-    pub fn put_url(this: &Cache, url: String, response: Response) -> ::js_sys::Promise;
+    pub fn put_url(this: &Cache, url: &str, response: &Response) -> ::js_sys::Promise;
 
     #[wasm_bindgen(structural, method, js_class=Cache, js_name=match)]
-    pub fn match_request(this: &Cache, request: Request, options: JsValue) -> ::js_sys::Promise;
+    pub fn match_request(this: &Cache, request: &Request, options: JsValue) -> ::js_sys::Promise;
 
     #[wasm_bindgen(structural, method, js_class=Cache, js_name=match)]
-    pub fn match_url(this: &Cache, url: String, options: JsValue) -> ::js_sys::Promise;
+    pub fn match_url(this: &Cache, url: &str, options: JsValue) -> ::js_sys::Promise;
 
     #[wasm_bindgen(structural, method, js_class=Cache, js_name=delete)]
-    pub fn delete_request(this: &Cache, request: Request, options: JsValue) -> ::js_sys::Promise;
+    pub fn delete_request(this: &Cache, request: &Request, options: JsValue) -> ::js_sys::Promise;
 
     #[wasm_bindgen(structural, method, js_class=Cache, js_name=delete)]
-    pub fn delete_url(this: &Cache, request: String, options: JsValue) -> ::js_sys::Promise;
+    pub fn delete_url(this: &Cache, request: &str, options: JsValue) -> ::js_sys::Promise;
 }
 
 // `caches` global object
