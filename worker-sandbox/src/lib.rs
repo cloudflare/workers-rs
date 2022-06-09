173819b7528ed150a3dce4a1f8733811650e70b4
Fisher Darling <fdarlingco@gmail.com>
2022-06-09 10:53:17 +0200

    attempt to add an alarm test

--- worker-sandbox/src/lib.rs
+++ worker-sandbox/src/lib.rs
@@ -347,6 +347,14 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
 
             Fetch::Url(url.parse()?).send().await
         })
+        .get_async("/durable/alarm", |_req, ctx| async move {
+            let namespace = ctx.durable_object("COUNTER")?;
+            let stub = namespace.id_from_name("alarm")?.get_stub()?;
+            // when calling fetch to a Durable Object, a full URL must be used. Alternatively, a
+            // compatibility flag can be provided in wrangler.toml to opt-in to older behavior:
+            // https://developers.cloudflare.com/workers/platform/compatibility-dates#durable-object-stubfetch-requires-a-full-url
+            stub.fetch_with_str("https://fake-host/alarm").await
+        })
         .get_async("/durable/:id", |_req, ctx| async move {
             let namespace = ctx.durable_object("COUNTER")?;
             let stub = namespace.id_from_name("A")?.get_stub()?;
