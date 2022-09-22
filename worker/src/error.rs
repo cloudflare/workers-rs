15a42b776b9313bba1e6a7389310a7e0f2e8064d
Caleb Moore <cmoore@darwinium.com>
2022-09-22 17:03:25 +1000

    Removed the usage of wasm_bindgen::JsValue::from_serde and wasm_bindgen::JsValue::into_serde that have been deprecated. Added the new serde_wasm_bindgen::from_value() and serde_wasm_bindgen::to_value() equivalents instead.

--- worker/src/error.rs
+++ worker/src/error.rs
@@ -29,6 +29,14 @@ impl From<url::ParseError> for Error {
     }
 }
 
+impl From<serde_wasm_bindgen::Error> for Error {
+    fn from(e: serde_wasm_bindgen::Error) -> Self {
+        let val: JsValue = e.into();
+        val.into()
+    }
+}
+
+
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
