9934527d4045422c50319a6823cdfa6ffe4f6543
FlareLine <1166637+FlareLine@users.noreply.github.com>
2023-01-20 19:42:13 +1100

    Added initial contributions with some fixes

--- worker-sys/src/types.rs
+++ worker-sys/src/types.rs
@@ -1,4 +1,5 @@
 mod context;
+mod d1;
 mod durable_object;
 mod dynamic_dispatcher;
 mod fetcher;
@@ -13,6 +14,7 @@ mod tls_client_auth;
 mod websocket_pair;
 
 pub use context::*;
+pub use d1::*;
 pub use durable_object::*;
 pub use dynamic_dispatcher::*;
 pub use fetcher::*;
