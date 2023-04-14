d2038d6272ba27fc184594f4c9adcf7896fb9c2c
Will Bartlett <wbartlett@cloudflare.com>
2023-04-14 15:44:48 +0100

    Exposes wait_until method on Durable Object state

--- worker-sys/src/types/durable_object/state.rs
+++ worker-sys/src/types/durable_object/state.rs
@@ -12,4 +12,7 @@ extern "C" {
 
     #[wasm_bindgen(method, getter)]
     pub fn storage(this: &DurableObjectState) -> DurableObjectStorage;
+
+    #[wasm_bindgen(method, js_name=waitUntil)]
+    pub fn wait_until(this: &DurableObjectState, promise: &js_sys::Promise);
 }
