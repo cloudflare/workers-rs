b1d4f3e507a110634d9cad803b04bc82041507cb
Kevin Flansburg <kflansburg@cloudflare.com>
2023-06-15 16:03:15 -0600

    tokio-postgres example

---
+++ examples/tokio-postgres/src/lib.rs
@@ -0,0 +1,25 @@
+use worker::*;
+
+#[event(fetch)]
+async fn main(_req: Request, _env: Env, _ctx: Context) -> Result<Response> {
+    let mut config = tokio_postgres::config::Config::new();
+    // Configure username, password, database as needed.
+    config.user("postgres");
+
+    // Connect using Worker Socket
+    let socket = Socket::builder().connect("database_url", 5432)?;
+    let (_client, connection) = config
+        .connect_raw(socket, tokio_postgres::tls::NoTls)
+        .await
+        .map_err(|e| worker::Error::RustError(format!("tokio-postgres: {:?}", e)))?;
+
+    wasm_bindgen_futures::spawn_local(async move {
+        if let Err(error) = connection.await {
+            console_log!("connection error: {:?}", error);
+        }
+    });
+
+    // Use `client` to make queries.
+
+    Response::ok("Hello, World!")
+}
--- examples/tracing/README.md
+++ examples/tracing/README.md
@@ -1,3 +1,3 @@
-# `hyper` on Cloudflare Workers
+# `tracing` on Cloudflare Workers
 
-Demonstration of using `hyper::client::conn` (the low-level client) on Cloudflare Workers using TCP Sockets.
+Demonstration of using `tracing-web` crate to enable the use of the `tracing` crate on Workers.
