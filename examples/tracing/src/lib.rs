3e451c42b90755a4e772304786989f72c0445017
Kevin Flansburg <kevin.flansburg@gmail.com>
2023-06-20 10:37:30 -0400

    Address comments

--- examples/tracing/src/lib.rs
+++ examples/tracing/src/lib.rs
@@ -14,7 +14,7 @@ fn test() {
 async fn main(req: Request, _env: Env, _ctx: Context) -> Result<Response> {
     let fmt_layer = tracing_subscriber::fmt::layer()
         .json()
-        .with_ansi(false) // Only partially supported across browsers
+        .with_ansi(false) // Only partially supported across JavaScript runtimes
         .with_timer(UtcTime::rfc_3339()) // std::time is not available in browsers
         .with_writer(MakeConsoleWriter); // write events to the console
     let perf_layer = performance_layer().with_details_from_fields(Pretty::default());
