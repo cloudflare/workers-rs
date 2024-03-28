ba5fc87663d3f19e7bcc6e83cb7676506691b89a
Roshan Patel <ovicior@gmail.com>
2024-03-28 01:32:29 -0400

    Add opened implementation

--- worker-sys/src/types/socket.rs
+++ worker-sys/src/types/socket.rs
@@ -18,6 +18,9 @@ extern "C" {
     #[wasm_bindgen(method)]
     pub fn closed(this: &Socket) -> js_sys::Promise;
 
+    #[wasm_bindgen(method)]
+    pub fn opened(this: &Socket) -> js_sys::Promise;
+
     #[wasm_bindgen(method, js_name=startTls)]
     pub fn start_tls(this: &Socket) -> Socket;
 
