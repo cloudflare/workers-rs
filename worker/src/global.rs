a842afa3032604440716a1f7995090a8b828186a
jjohnson <jjohnson@cloudflare.com>
2024-04-23 12:25:31 -0500

    Add `http` feature support to global `Fetch`

--- worker/src/global.rs
+++ worker/src/global.rs
@@ -3,20 +3,32 @@ use std::ops::Deref;
 use wasm_bindgen::JsCast;
 use wasm_bindgen_futures::JsFuture;
 
-use crate::{
-    request::Request as WorkerRequest, response::Response as WorkerResponse, AbortSignal, Result,
-};
+use crate::{AbortSignal, Result};
+
+#[cfg(feature = "http")]
+use crate::{HttpRequest, HttpResponse};
+use crate::{Request, Response};
+
+#[cfg(not(feature = "http"))]
+type FetchResponseType = Response;
+#[cfg(feature = "http")]
+type FetchResponseType = HttpResponse;
+
+#[cfg(not(feature = "http"))]
+type FetchRequestType = Request;
+#[cfg(feature = "http")]
+type FetchRequestType = HttpRequest;
 
 /// Construct a Fetch call from a URL string or a Request object. Call its `send` method to execute
 /// the request.
 pub enum Fetch {
     Url(url::Url),
-    Request(WorkerRequest),
+    Request(FetchRequestType),
 }
 
 impl Fetch {
     /// Execute a Fetch call and receive a Response.
-    pub async fn send(&self) -> Result<WorkerResponse> {
+    pub async fn send(&self) -> Result<FetchResponseType> {
         match self {
             Fetch::Url(url) => fetch_with_str(url.as_ref(), None).await,
             Fetch::Request(req) => fetch_with_request(req, None).await,
@@ -24,7 +36,7 @@ impl Fetch {
     }
 
     /// Execute a Fetch call and receive a Response.
-    pub async fn send_with_signal(&self, signal: &AbortSignal) -> Result<WorkerResponse> {
+    pub async fn send_with_signal(&self, signal: &AbortSignal) -> Result<FetchResponseType> {
         match self {
             Fetch::Url(url) => fetch_with_str(url.as_ref(), Some(signal)).await,
             Fetch::Request(req) => fetch_with_request(req, Some(signal)).await,
@@ -32,28 +44,41 @@ impl Fetch {
     }
 }
 
-async fn fetch_with_str(url: &str, signal: Option<&AbortSignal>) -> Result<WorkerResponse> {
+async fn fetch_with_str(url: &str, signal: Option<&AbortSignal>) -> Result<FetchResponseType> {
     let mut init = web_sys::RequestInit::new();
     init.signal(signal.map(|x| x.deref()));
 
     let worker: web_sys::WorkerGlobalScope = js_sys::global().unchecked_into();
     let promise = worker.fetch_with_str_and_init(url, &init);
-    let resp = JsFuture::from(promise).await?;
-    let resp: web_sys::Response = resp.dyn_into()?;
-    Ok(resp.into())
+    let resp: web_sys::Response = JsFuture::from(promise).await?.dyn_into()?;
+    #[cfg(not(feature = "http"))]
+    let result = Ok(Response::from(resp));
+    #[cfg(feature = "http")]
+    let result = crate::response_from_wasm(resp_sys);
+    result
 }
 
 async fn fetch_with_request(
-    request: &WorkerRequest,
+    request: &FetchRequestType,
     signal: Option<&AbortSignal>,
-) -> Result<WorkerResponse> {
+) -> Result<FetchResponseType> {
     let mut init = web_sys::RequestInit::new();
     init.signal(signal.map(|x| x.deref()));
+    #[cfg(feature = "http")]
+    let req = TryInto::<Request>::try_into(request)?;
+    #[cfg(not(feature = "http"))]
+    let req = request;
 
     let worker: web_sys::WorkerGlobalScope = js_sys::global().unchecked_into();
-    let req = request.inner();
+    let req = req.inner();
     let promise = worker.fetch_with_request_and_init(req, &init);
-    let resp = JsFuture::from(promise).await?;
-    let edge_response: web_sys::Response = resp.dyn_into()?;
-    Ok(edge_response.into())
+    let resp_sys: web_sys::Response = JsFuture::from(promise).await?.dyn_into()?;
+    let response = Response::from(resp_sys);
+
+    #[cfg(feature = "http")]
+    let result = response.try_into();
+    #[cfg(not(feature = "http"))]
+    let result = Ok(response);
+
+    result
 }
