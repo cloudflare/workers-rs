0fd2ff1cc68107fddc30ba8e6a81de1ada59d2ed
Kevin Flansburg <kevin.flansburg@gmail.com>
2024-04-28 18:35:15 -0400

    Final tweaks

--- examples/rpc-client/build.rs
+++ examples/rpc-client/build.rs
@@ -4,7 +4,7 @@ use worker_codegen::wit::expand_wit_source;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Tell Cargo that if the given file changes, to rerun this build script.
-    println!("cargo::rerun-if-changed=calculator.wit");
+    println!("cargo::rerun-if-changed=wit/calculator.wit");
     let source = expand_wit_source("wit/calculator.wit")?;
     let out_dir = var("OUT_DIR")?;
     let dest = format!("{}/calculator.rs", out_dir);
