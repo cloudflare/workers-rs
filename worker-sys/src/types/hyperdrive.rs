0ff79fd2bd0a9d87c0ef40aebcb15a678eee9a92
Alexander van Saase <avsaase@gmail.com>
2024-05-03 20:52:05 +0200

    Add Hyperdrive getters

--- worker-sys/src/types/hyperdrive.rs
+++ worker-sys/src/types/hyperdrive.rs
@@ -5,4 +5,22 @@ extern "C" {
     #[wasm_bindgen(extends=js_sys::Object)]
     #[derive(Clone, PartialEq, Eq)]
     pub type Hyperdrive;
+
+    #[wasm_bindgen(method, getter, js_name=connectionString)]
+    pub fn connection_string(this: &Hyperdrive) -> String;
+
+    #[wasm_bindgen(method, getter, js_name=host)]
+    pub fn host(this: &Hyperdrive) -> String;
+
+    #[wasm_bindgen(method, getter, js_name=port)]
+    pub fn port(this: &Hyperdrive) -> u16;
+
+    #[wasm_bindgen(method, getter, js_name=user)]
+    pub fn user(this: &Hyperdrive) -> String;
+
+    #[wasm_bindgen(method, getter, js_name=password)]
+    pub fn password(this: &Hyperdrive) -> String;
+
+    #[wasm_bindgen(method, getter, js_name=database)]
+    pub fn database(this: &Hyperdrive) -> String;
 }
