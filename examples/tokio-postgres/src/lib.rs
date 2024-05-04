3f1f28e48ab5b8e8ea0a701ddc63e0e2ed784357
Kevin Flansburg <kflansburg@cloudflare.com>
2024-05-04 09:29:18 -0400

    Update tokio-postgres to demonstrate Hyperdrive

--- examples/tokio-postgres/src/lib.rs
+++ examples/tokio-postgres/src/lib.rs
@@ -2,19 +2,19 @@ use worker::postgres_tls::PassthroughTls;
 use worker::*;
 
 #[event(fetch)]
-async fn main(_req: Request, _env: Env, _ctx: Context) -> Result<Response> {
-    let mut config = tokio_postgres::config::Config::new();
-    // Configure username, password, database as needed.
-    config.user("postgres");
+async fn main(_req: Request, env: Env, _ctx: Context) -> anyhow::Result<Response> {
+    let hyperdrive = env.hyperdrive("DB")?;
 
     // Connect using Worker Socket
     let socket = Socket::builder()
         .secure_transport(SecureTransport::StartTls)
-        .connect("database_url", 5432)?;
-    let (_client, connection) = config
-        .connect_raw(socket, PassthroughTls)
-        .await
-        .map_err(|e| worker::Error::RustError(format!("tokio-postgres: {:?}", e)))?;
+        .connect(hyperdrive.host(), hyperdrive.port())?;
+
+    let config = hyperdrive
+        .connection_string()
+        .parse::<tokio_postgres::Config>()?;
+
+    let (client, connection) = config.connect_raw(socket, PassthroughTls).await?;
 
     wasm_bindgen_futures::spawn_local(async move {
         if let Err(error) = connection.await {
@@ -22,7 +22,16 @@ async fn main(_req: Request, _env: Env, _ctx: Context) -> Result<Response> {
         }
     });
 
-    // Use `client` to make queries.
+    // Setup table:
+    // CREATE TABLE IF NOT EXISTS foo (id SERIAL PRIMARY KEY, name TEXT);
+    // INSERT INTO foo (name) VALUES ('Fred');
+    let result = client.query("SELECT * FROM FOO", &[]).await?;
 
-    Response::ok("Hello, World!")
+    for row in result {
+        let id: i32 = row.get(0);
+        let name: &str = row.get(1);
+        console_log!("id: {}, name: {}", id, name);
+    }
+
+    Ok(Response::ok("Hello, World!")?)
 }
