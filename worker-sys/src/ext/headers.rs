46cfd2f66f75c92be289ed43bba9f071f516c4ed
nakamura.shuta@classmethod.jp <nakamura.shuta@classmethod.jp>
2024-09-02 10:26:59 +0900

    Included in glue module along with other modules

--- worker-sys/src/ext/headers.rs
+++ worker-sys/src/ext/headers.rs
@@ -1,12 +1,16 @@
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(extends = js_sys::Object)]
-    pub type Headers;
+mod glue {
+    use super::*;
 
-    #[wasm_bindgen(method, js_name = getAll)]
-    pub fn get_all(this: &Headers, name: &str) -> js_sys::Array;
+    #[wasm_bindgen]
+    extern "C" {
+        #[wasm_bindgen(extends = js_sys::Object)]
+        pub type Headers;
+
+        #[wasm_bindgen(method, js_name = getAll)]
+        pub fn get_all(this: &Headers, name: &str) -> js_sys::Array;
+    }
 }
 
 pub trait HeadersExt {
@@ -15,6 +19,6 @@ pub trait HeadersExt {
 
 impl HeadersExt for web_sys::Headers {
     fn get_all(&self, name: &str) -> js_sys::Array {
-        self.unchecked_ref::<Headers>().get_all(name)
+        self.unchecked_ref::<glue::Headers>().get_all(name)
     }
 }
