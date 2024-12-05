a0a56e9875cf26556f84e2d11a01ef80b3ee42d7
Zeb Piasecki <zeb@cloudflare.com>
2024-12-05 08:58:25 -0500

    fix: call module initializer to setup externref table

--- worker-build/src/js/shim.js
+++ worker-build/src/js/shim.js
@@ -10,44 +10,44 @@ const instance = new WebAssembly.Instance(wasmModule, {
 imports.__wbg_set_wasm(instance.exports);
 
 // Run the worker's initialization function.
-imports.start?.();
+instance.exports.__wbindgen_start?.();
 
 export { wasmModule };
 
 class Entrypoint extends WorkerEntrypoint {
-    async fetch(request) {
-        let response = imports.fetch(request, this.env, this.ctx);
-        $WAIT_UNTIL_RESPONSE
-        return await response;
-    }
+	async fetch(request) {
+		let response = imports.fetch(request, this.env, this.ctx);
+		$WAIT_UNTIL_RESPONSE;
+		return await response;
+	}
 
-    async queue(batch) {
-        return await imports.queue(batch, this.env, this.ctx)
-    }
+	async queue(batch) {
+		return await imports.queue(batch, this.env, this.ctx);
+	}
 
-    async scheduled(event) {
-        return await imports.scheduled(event, this.env, this.ctx)
-    }
+	async scheduled(event) {
+		return await imports.scheduled(event, this.env, this.ctx);
+	}
 }
 
 const EXCLUDE_EXPORT = [
-    "IntoUnderlyingByteSource",
-    "IntoUnderlyingSink",
-    "IntoUnderlyingSource",
-    "MinifyConfig",
-    "PolishConfig",
-    "R2Range",
-    "RequestRedirect",
-    "fetch",
-    "queue",
-    "scheduled",
-    "getMemory"
+	"IntoUnderlyingByteSource",
+	"IntoUnderlyingSink",
+	"IntoUnderlyingSource",
+	"MinifyConfig",
+	"PolishConfig",
+	"R2Range",
+	"RequestRedirect",
+	"fetch",
+	"queue",
+	"scheduled",
+	"getMemory",
 ];
 
-Object.keys(imports).map(k => {
-    if (!(EXCLUDE_EXPORT.includes(k) | k.startsWith("__"))) {
-        Entrypoint.prototype[k] = imports[k];
-    }
-})
+Object.keys(imports).map((k) => {
+	if (!(EXCLUDE_EXPORT.includes(k) | k.startsWith("__"))) {
+		Entrypoint.prototype[k] = imports[k];
+	}
+});
 
 export default Entrypoint;
