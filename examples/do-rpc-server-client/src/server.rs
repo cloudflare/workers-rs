e8a56ea2d40354acce56f4ce7cda7ca4ed17ccda
Alexandre Faria <alexandre.faria@auroka.pt>
2025-05-09 21:26:09 +0100

    examples do-rpc-server-client: Clean up.

--- examples/do-rpc-server-client/src/server.rs
+++ examples/do-rpc-server-client/src/server.rs
@@ -22,26 +22,4 @@ impl DurableObject for RPCServer {
     fn new(state: State, _env: Env) -> Self {
         Self { state, counter: 0 }
     }
-
-    // URL /{left}+{right}
-    async fn fetch(&mut self, req: Request) -> Result<Response> {
-        console_error_panic_hook::set_once();
-        let url = req.url()?;
-        let path = url.path();
-
-        let path = path.trim_start_matches('/');
-
-        let path_parts: Vec<&str> = path.split('+').collect();
-        if path_parts.len() != 2 {
-            return Response::error("Invalid URL", 400);
-        }
-
-        if let (Ok(left), Ok(right)) = (path_parts[0].parse::<u32>(), path_parts[1].parse::<u32>())
-        {
-            let result = self.add(left, right);
-            Response::ok(&format!("{} + {} = {}", left, right, result))
-        } else {
-            return Response::error("Invalid operands", 400);
-        }
-    }
 }
