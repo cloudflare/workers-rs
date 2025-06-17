8bbfe4cd51a49bad9ea5c77eced8767ab2b305e5
Guy Bedford <gbedford@cloudflare.com>
2025-06-17 13:19:05 -0700

    Breaking Change: Use non-mutable methods for DurableObject, FormData, Headers & Response (#753)

--- worker-sandbox/src/test/put_raw.rs
+++ worker-sandbox/src/test/put_raw.rs
@@ -6,8 +6,8 @@ pub struct PutRawTestObject {
 }
 
 impl PutRawTestObject {
-    async fn put_raw(&mut self) -> Result<()> {
-        let mut storage = self.state.storage();
+    async fn put_raw(&self) -> Result<()> {
+        let storage = self.state.storage();
         let bytes = Uint8Array::new_with_length(3);
         bytes.copy_from(b"123");
         storage.put_raw("bytes", bytes).await?;
@@ -20,8 +20,8 @@ impl PutRawTestObject {
         Ok(())
     }
 
-    async fn put_multiple_raw(&mut self) -> Result<()> {
-        let mut storage = self.state.storage();
+    async fn put_multiple_raw(&self) -> Result<()> {
+        let storage = self.state.storage();
         let obj = js_sys::Object::new();
         const BAR: &[u8] = b"bar";
         let value = Uint8Array::new_with_length(BAR.len() as _);
@@ -45,7 +45,7 @@ impl DurableObject for PutRawTestObject {
         Self { state }
     }
 
-    async fn fetch(&mut self, _: Request) -> Result<Response> {
+    async fn fetch(&self, _: Request) -> Result<Response> {
         self.put_raw().await?;
         self.put_multiple_raw().await?;
 
--- worker-sandbox/tests/durable.spec.ts
+++ worker-sandbox/tests/durable.spec.ts
@@ -43,4 +43,3 @@ describe("durable", () => {
     expect(closeHandlerWrapper).toBeCalled();
   });
 });
-
