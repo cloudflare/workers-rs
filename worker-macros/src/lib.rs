omarabid/workers-plus

improve DurableObject support

worker-macros/src/lib.rs
@@ -4,9 +4,66 @@ mod send;
 
 use proc_macro::TokenStream;
 
+/// Integrate the struct with the Workers Runtime as Durable Object.\
+/// This requires to impl `DurableObject` trait and the trait requires this attribute.
+/// 
+/// ## Example
+/// 
+/// ```rust
+/// #[DurableObject]
+/// pub struct Chatroom {
+///     users: Vec<User>,
+///     messages: Vec<Message>,
+///     state: State,
+///     env: Env, // access `Env` across requests, use inside `fetch`
+/// }
+/// 
+/// impl DurableObject for Chatroom {
+///     fn new(state: State, env: Env) -> Self {
+///         Self {
+///             users: vec![],
+///             messages: vec![],
+///             state,
+///             env,
+///         }
+///     }
+/// 
+///     async fn fetch(&mut self, _req: Request) -> Result<Response> {
+///         // do some work when a worker makes a request to this DO
+///         Response::ok(&format!("{} active users.", self.users.len()))
+///     }
+/// }
+/// ```
+/// 
+/// ## Note
+/// 
+/// You can specify the usage of the Durable Object via argument to control wasm outout:
+/// 
+/// * `fetch`: simple `fetch` target
+/// * `alarm`: with [Alarms API](https://developers.cloudflare.com/durable-objects/examples/alarms-api/)
+/// * `websocket`: [WebSocket server](https://developers.cloudflare.com/durable-objects/examples/websocket-hibernation-server/)
+/// 
+/// ```rust
+/// #[DurableObject(fetch)]
+/// pub struct Chatroom {
+///     users: Vec<User>,
+///     messages: Vec<Message>,
+///     state: State,
+///     env: Env, // access `Env` across requests, use inside `fetch`
+/// }
+/// ```
 #[proc_macro_attribute]
-pub fn durable_object(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    durable_object::expand_macro(item.into())
+#[allow(non_snake_case)]
+pub fn DurableObject(attr: TokenStream, item: TokenStream) -> TokenStream {
+    durable_object::expand_macro(attr.into(), item.into())
+        .unwrap_or_else(syn::Error::into_compile_error)
+        .into()
+}
+
+#[deprecated = "use `#[DurableObject]` instead"]
+#[proc_macro_attribute]
+pub fn durable_object(_: TokenStream, item: TokenStream) -> TokenStream {
+    durable_object::expand_macro(TokenStream::new().into(), item.into())
         .unwrap_or_else(syn::Error::into_compile_error)
         .into()
 }
@@ -85,4 +142,10 @@ pub fn event(attr: TokenStream, item: TokenStream) -> TokenStream {
 /// ```
 pub fn send(attr: TokenStream, stream: TokenStream) -> TokenStream {
     send::expand_macro(attr, stream)
+}
+
+#[doc(hidden)]
+#[proc_macro_attribute]
+pub fn consume(_: TokenStream, _: TokenStream) -> TokenStream {
+    TokenStream::new()
 }

cloudflare/workers-rs

worker-macros/src/lib.rs
@@ -4,9 +4,58 @@ mod send;
 
 use proc_macro::TokenStream;
 
+/// Integrate the struct with the Workers Runtime as Durable Object.\
+/// This requires to impl `DurableObject` trait and the trait requires the durable_object attribute.
+/// 
+/// ## Example
+/// 
+/// ```rust
+/// #[durable_object]
+/// pub struct Chatroom {
+///     users: Vec<User>,
+///     messages: Vec<Message>,
+///     state: State,
+///     env: Env, // access `Env` across requests, use inside `fetch`
+/// }
+/// 
+/// impl DurableObject for Chatroom {
+///     fn new(state: State, env: Env) -> Self {
+///         Self {
+///             users: vec![],
+///             messages: vec![],
+///             state,
+///             env,
+///         }
+///     }
+/// 
+///     async fn fetch(&self, _req: Request) -> Result<Response> {
+///         // do some work when a worker makes a request to this DO
+///         Response::ok(&format!("{} active users.", self.users.len()))
+///     }
+/// }
+/// ```
+/// 
+/// ## Note
+/// 
+/// You can specify the usage of the Durable Object via argument to control wasm outout:
+/// 
+/// * `fetch`: simple `fetch` target
+/// * `alarm`: with [Alarms API](https://developers.cloudflare.com/durable-objects/examples/alarms-api/)
+/// * `websocket`: [WebSocket server](https://developers.cloudflare.com/durable-objects/examples/websocket-hibernation-server/)
+/// 
+/// ```rust
+/// #[durable_object(fetch)]
+/// pub struct Chatroom {
+///     users: Vec<User>,
+///     messages: Vec<Message>,
+///     state: State,
+///     env: Env, // access `Env` across requests, use inside `fetch`
+/// }
+/// ```
 #[proc_macro_attribute]
-pub fn durable_object(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    durable_object::expand_macro(item.into())
+#[allow(non_snake_case)]
+pub fn durable_object(attr: TokenStream, item: TokenStream) -> TokenStream {
+    durable_object::expand_macro(attr.into(), item.into())
         .unwrap_or_else(syn::Error::into_compile_error)
         .into()
 }
@@ -85,4 +134,10 @@ pub fn event(attr: TokenStream, item: TokenStream) -> TokenStream {
 /// ```
 pub fn send(attr: TokenStream, stream: TokenStream) -> TokenStream {
     send::expand_macro(attr, stream)
+}
+
+#[doc(hidden)]
+#[proc_macro_attribute]
+pub fn consume(_: TokenStream, _: TokenStream) -> TokenStream {
+    TokenStream::new()
 }