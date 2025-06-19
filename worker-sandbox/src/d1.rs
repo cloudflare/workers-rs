141551ae6ec0fb623082a14f2237db7af34d79d0
Alexandre Faria <alexandre.faria@auroka.pt>
2025-06-19 01:10:55 +0100

    test: D1 Binding - Null Support Checks (#680)

--- worker-sandbox/src/d1.rs
+++ worker-sandbox/src/d1.rs
@@ -1,6 +1,11 @@
 use crate::SomeSharedData;
+use crate::{
+    js_sys::{Object, Reflect},
+    wasm_bindgen,
+};
 use serde::Deserialize;
-use worker::*;
+use wasm_bindgen::JsValue;
+use worker::{D1PreparedArgument, D1Type, Env, Error, Request, Response, Result};
 
 #[derive(Deserialize)]
 struct Person {
@@ -124,4 +129,179 @@ pub async fn error(_req: Request, env: Env, _data: SomeSharedData) -> Result<Res
     }
 
     Response::ok("")
+}
+
+#[derive(Debug, Deserialize)]
+struct NullablePerson {
+    id: u32,
+    name: Option<String>,
+    age: Option<u32>,
+}
+
+#[worker::send]
+pub async fn jsvalue_null_is_null(
+    _req: Request,
+    _env: Env,
+    _data: SomeSharedData,
+) -> Result<Response> {
+    console_error_panic_hook::set_once();
+
+    assert!(wasm_bindgen::JsValue::NULL.is_null());
+
+    Response::ok("ok")
+}
+
+#[worker::send]
+pub async fn serialize_optional_none(
+    _req: Request,
+    _env: Env,
+    _data: SomeSharedData,
+) -> Result<Response> {
+    console_error_panic_hook::set_once();
+    let serializer = serde_wasm_bindgen::Serializer::new().serialize_missing_as_null(true);
+
+    let none: Option<String> = None;
+    let js_none = ::serde::ser::Serialize::serialize(&none, &serializer).unwrap();
+    assert!(js_none.is_null());
+
+    Response::ok("ok")
+}
+
+#[worker::send]
+pub async fn serialize_optional_some(
+    _req: Request,
+    _env: Env,
+    _data: SomeSharedData,
+) -> Result<Response> {
+    console_error_panic_hook::set_once();
+    let serializer = serde_wasm_bindgen::Serializer::new().serialize_missing_as_null(true);
+
+    let some: Option<String> = Some("Hello".to_string());
+    let js_some = ::serde::ser::Serialize::serialize(&some, &serializer).unwrap();
+    assert!(js_some.is_string());
+
+    Response::ok("ok")
+}
+
+#[worker::send]
+pub async fn deserialize_optional_none(
+    _req: Request,
+    _env: Env,
+    _data: SomeSharedData,
+) -> Result<Response> {
+    console_error_panic_hook::set_once();
+
+    let js_value = Object::new();
+    Reflect::set(&js_value, &JsValue::from_str("id"), &JsValue::from_f64(1.0)).unwrap();
+    Reflect::set(&js_value, &JsValue::from_str("name"), &JsValue::NULL).unwrap();
+    Reflect::set(&js_value, &JsValue::from_str("age"), &JsValue::NULL).unwrap();
+
+    let js_value: JsValue = js_value.into();
+
+    let value: NullablePerson = serde_wasm_bindgen::from_value(js_value).unwrap();
+
+    assert_eq!(value.id, 1);
+    assert_eq!(value.name, None);
+    assert_eq!(value.age, None);
+
+    Response::ok("ok")
+}
+
+#[worker::send]
+pub async fn insert_and_retrieve_optional_none(
+    _req: Request,
+    env: Env,
+    _data: SomeSharedData,
+) -> Result<Response> {
+    let db = env.d1("DB")?;
+
+    let query = worker::query!(
+        &db,
+        "INSERT INTO nullable_people (id, name, age) VALUES (?1, ?2, ?3)",
+        &3,
+        &None::<String>,
+        &None::<u32>
+    )?;
+    query.run().await?;
+
+    let stmt = worker::query!(&db, "SELECT * FROM nullable_people WHERE id = 3");
+    let person = stmt.first::<NullablePerson>(None).await?.unwrap();
+    assert_eq!(person.id, 3);
+    assert_eq!(person.name, None);
+    assert_eq!(person.age, None);
+
+    Response::ok("ok")
+}
+
+#[worker::send]
+pub async fn insert_and_retrieve_optional_some(
+    _req: Request,
+    env: Env,
+    _data: SomeSharedData,
+) -> Result<Response> {
+    let db = env.d1("DB")?;
+    let query = worker::query!(
+        &db,
+        "INSERT INTO nullable_people (id, name, age) VALUES (?1, ?2, ?3)",
+        &4,
+        &"Dude",
+        &12
+    )?;
+    query.run().await?;
+
+    let stmt = worker::query!(&db, "SELECT * FROM nullable_people WHERE id = 4");
+    let person = stmt.first::<NullablePerson>(None).await?.unwrap();
+    assert_eq!(person.id, 4);
+    assert_eq!(person.name, Some("Dude".to_string()));
+    assert_eq!(person.age, Some(12));
+
+    Response::ok("ok")
+}
+
+#[worker::send]
+pub async fn retrieve_optional_none(
+    _req: Request,
+    env: Env,
+    _data: SomeSharedData,
+) -> Result<Response> {
+    let db = env.d1("DB")?;
+
+    let stmt = worker::query!(&db, "SELECT * FROM nullable_people WHERE id = 1");
+    let person = stmt.first::<NullablePerson>(None).await?.unwrap();
+    assert_eq!(person.id, 1);
+    assert_eq!(person.name, None);
+    assert_eq!(person.age, None);
+
+    Response::ok("ok")
+}
+
+#[worker::send]
+pub async fn retrieve_optional_some(
+    _req: Request,
+    env: Env,
+    _data: SomeSharedData,
+) -> Result<Response> {
+    let db = env.d1("DB")?;
+
+    let stmt = worker::query!(&db, "SELECT * FROM nullable_people WHERE id = 2");
+    let person = stmt.first::<NullablePerson>(None).await?.unwrap();
+    assert_eq!(person.id, 2);
+    assert_eq!(person.name, Some("Wynne Ogley".to_string()));
+    assert_eq!(person.age, Some(67));
+
+    Response::ok("ok")
+}
+
+#[worker::send]
+pub async fn retrive_first_none(
+    _req: Request,
+    env: Env,
+    _data: SomeSharedData,
+) -> Result<Response> {
+    let db = env.d1("DB")?;
+
+    let stmt = worker::query!(&db, "SELECT * FROM nullable_people WHERE id = 9999");
+    assert!(stmt.first::<NullablePerson>(None).await?.is_none());
+
+    Response::ok("ok")
 }
