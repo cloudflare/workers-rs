baecae1f729ef520ecb20b57e9f7d312aa69b9a1
parzivale <40600864+parzivale@users.noreply.github.com>
2025-10-08 22:45:20 +0200

    tweak: moved example to use worker type

--- test/src/ai.rs
+++ test/src/ai.rs
@@ -1,21 +1,9 @@
 use worker::{
-    worker_sys::{AiTextGenerationInput, AiTextGenerationOutput},
-    Env, Model, Request, Response, Result, StreamableModel,
+    models::llama_4_scout_17b_16e_instruct::Llama4Scout17b16eInstruct,
+    worker_sys::AiTextGenerationInput, Env, Request, Response, Result,
 };
 
 use crate::SomeSharedData;
-
-pub struct Llama4Scout17b16eInstruct;
-
-impl Model for Llama4Scout17b16eInstruct {
-    const MODEL_NAME: &str = "@cf/meta/llama-4-scout-17b-16e-instruct";
-
-    type Input = AiTextGenerationInput;
-
-    type Output = AiTextGenerationOutput;
-}
-
-impl StreamableModel for Llama4Scout17b16eInstruct {}
 
 const AI_TEST: &str = "AI_TEST";
 
