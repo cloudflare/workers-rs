4080689fe933fc3218d4751cfb0017dd9219a9b6
parzivale <40600864+parzivale@users.noreply.github.com>
2025-10-10 23:30:25 +0200

    fix: potentially fixes ci errors, accidental extra slash

--- test/tests/ai.spec.ts
+++ test/tests/ai.spec.ts
@@ -2,10 +2,10 @@ import { describe, expect, test } from "vitest";
 import { mf, mfUrl } from "./mf";
 
 async function runTest() {
-  let normal_response = await mf.dispatchFetch(`${mfUrl}/ai`);
+  let normal_response = await mf.dispatchFetch(`${mfUrl}ai`);
   expect(normal_response.status).toBe(200);
 
-  let streaming_response = await mf.dispatchFetch(`${mfUrl}/ai/streaming`);
+  let streaming_response = await mf.dispatchFetch(`${mfUrl}ai/streaming`);
   expect(streaming_response.status).toBe(200);
 }
 describe("ai", runTest);
