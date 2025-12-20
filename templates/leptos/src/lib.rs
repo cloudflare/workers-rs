4f032d6d08956e8d3e869eb8e0403cc091f7e89f
rafiyq <rafiyqw@gmail.com>
2025-12-21 00:30:36 +0700

    chore: remove unnecessary code (#887)

--- templates/leptos/src/lib.rs
+++ templates/leptos/src/lib.rs
@@ -1,56 +1,41 @@
-#[cfg(feature = "ssr")]
-use worker::*;
-
-use crate::app::*;
-
-pub mod app;
+mod app;
+mod api;
 mod components;
 
 #[cfg(feature = "ssr")]
-pub fn register_server_functions() {
-    use leptos::server_fn::axum::register_explicit;
-
-    // Add all of your server functions here
-    register_explicit::<components::show_data_from_api::SayHello>();
-}
-
-#[cfg(feature = "ssr")]
-async fn router(env: Env) -> axum::Router {
+#[worker::event(fetch)]
+async fn fetch(
+    req: worker::HttpRequest,
+    env: worker::Env,
+    _ctx: worker::Context,
+) -> worker::Result<axum::http::Response<axum::body::Body>> {
     use std::sync::Arc;
 
     use axum::{Extension, Router};
     use leptos::prelude::*;
     use leptos_axum::{generate_route_list, LeptosRoutes};
+    use tower_service::Service;
 
+    use app::{App, shell};
+    
     let conf = get_configuration(None).unwrap();
     let leptos_options = conf.leptos_options;
     let routes = generate_route_list(App);
-    register_server_functions();
 
     // build our application with a route
-    Router::new()
+    let mut router = Router::new()
         .leptos_routes(&leptos_options, routes, {
             let leptos_options = leptos_options.clone();
             move || shell(leptos_options.clone())
         })
         .with_state(leptos_options)
-        .layer(Extension(Arc::new(env))) // <- Allow leptos server functions to access Worker stuff
-}
+        .layer(Extension(Arc::new(env))); // <- Allow leptos server functions to access Worker stuff
 
-#[cfg(feature = "ssr")]
-#[event(fetch)]
-async fn fetch(
-    req: HttpRequest,
-    env: Env,
-    _ctx: Context,
-) -> Result<axum::http::Response<axum::body::Body>> {
-    use tower_service::Service;
-
-    Ok(router(env).await.call(req).await?)
+    Ok(router.call(req).await?)
 }
 
 #[cfg(feature = "hydrate")]
 #[wasm_bindgen::prelude::wasm_bindgen]
 pub fn hydrate() {
-    leptos::mount::hydrate_body(App);
+    leptos::mount::hydrate_body(app::App);
 }
