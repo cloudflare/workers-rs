5079c471bc9bc13df4f151b2d3112d74d59d7ceb
Eric Otuvaka <eric.otuvaka@gmail.com>
2026-01-05 17:15:18 -0800

    wip: foo service and impl

--- examples/axum/src/lib.rs
+++ examples/axum/src/lib.rs
@@ -2,8 +2,18 @@ use axum::{routing::get, Router};
 use tower_service::Service;
 use worker::*;
 
+struct AppState {
+    kv: KvStore
+}
+
 fn router() -> Router {
-    Router::new().route("/", get(root))
+    let kv = env.kv("EXAMPLE")?;
+    let foo_service =
+
+    let app_state = AppState {
+        kv
+    };
+    Router::new().route("/", get(root).route("/foo", get(foo_api))).with_state(app_state)
 }
 
 #[event(fetch)]
@@ -17,4 +27,27 @@ async fn fetch(
 
 pub async fn root() -> &'static str {
     "Hello Axum!"
+}
+
+#[derive(Serialize)]
+struct Foo {
+    id: String
+    msg: String
+}
+
+pub async fn foo_api(State(state): State<App>, Path(foo_id): Path(String)) -> Result<axum::http::Response<Foo>> {
+    let foo = state.foo_service.get(foo_id).await
+}
+
+struct FooService {
+    kv: KvStore
+}
+
+impl FooService {
+    pub async fn get(foo_id: String) -> Option<Foo> {
+        if let Some(q) = self.cache.get::<Foo>(&foo_id).await? {
+            return Ok(q);
+        }
+        None
+    }
 }
