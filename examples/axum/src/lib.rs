402f0507e3b18977d83bcdbf4bb5da6d325ee9d1
Eric <eric.otuvaka@gmail.com>
2026-01-12 12:45:04 -0800

    docs: axum advanced example (#903)

--- examples/axum/src/lib.rs
+++ examples/axum/src/lib.rs
@@ -1,18 +1,40 @@
 use axum::{routing::get, Router};
+use std::sync::Arc;
 use tower_service::Service;
 use worker::*;
 
-fn router() -> Router {
-    Router::new().route("/", get(root))
+pub mod error;
+pub mod resources;
+
+use crate::resources::foos::{self, service::FooService};
+
+/// AppState's readonly fields are all `Arc<T>` for safe sharing between threads
+#[derive(Clone)]
+struct AppState {
+    foo_service: Arc<FooService>,
+}
+
+fn router(env: Env) -> Router {
+    let kv = env.kv("EXAMPLE").unwrap();
+    let foo_service = FooService::new(kv);
+
+    let app_state = AppState {
+        foo_service: Arc::new(foo_service),
+    };
+
+    Router::new()
+        .route("/", get(root))
+        .route("/foo", get(foos::api::get))
+        .with_state(app_state)
 }
 
 #[event(fetch)]
 async fn fetch(
     req: HttpRequest,
-    _env: Env,
+    env: Env,
     _ctx: Context,
 ) -> Result<axum::http::Response<axum::body::Body>> {
-    Ok(router().call(req).await?)
+    Ok(router(env).call(req).await?)
 }
 
 pub async fn root() -> &'static str {
