695dccebe1bd5d9e1f5d620c67e480f0aade77e2
Guy Bedford <gbedford@cloudflare.com>
2026-02-20 16:22:49 -0800

    deps bump

--- worker-build/src/versions.rs
+++ worker-build/src/versions.rs
@@ -7,13 +7,13 @@ macro_rules! version {
 }
 
 // Current build toolchain, always used exactly for builds, unless overridden by {}_BIN env vars
-pub(crate) static LATEST_WASM_BINDGEN_VERSION: LazyLock<semver::Version> = version!("0.2.108");
-pub(crate) static CUR_WASM_OPT_VERSION: &str = "125";
-pub(crate) static CUR_ESBUILD_VERSION: LazyLock<semver::Version> = version!("0.27.2");
+pub(crate) static LATEST_WASM_BINDGEN_VERSION: LazyLock<semver::Version> = version!("0.2.110");
+pub(crate) static CUR_WASM_OPT_VERSION: &str = "126";
+pub(crate) static CUR_ESBUILD_VERSION: LazyLock<semver::Version> = version!("0.27.3");
 
 // Minimum required libraries, validated before build
 pub(crate) static MIN_WASM_BINDGEN_LIB_VERSION: LazyLock<semver::Version> = version!("0.2.106");
-pub(crate) static MIN_RUSTC_VERSION: LazyLock<semver::Version> = version!("1.71.0"); // wasm-bindgen MSRV
+pub(crate) static MIN_RUSTC_VERSION: LazyLock<semver::Version> = version!("1.71.0"); // workers-rs MSRV
 
 pub(crate) static MIN_WORKER_LIB_VERSION: LazyLock<semver::Version> = version!(&format!(
     "{}.0",
