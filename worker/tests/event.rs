168f070ece89cf8a82f2c0a9cf770e4e2d357a35
Jacob Adam <software@jacobadam.net>
2026-04-12 16:15:07 +0100

    Add a regression test

--- worker/tests/event.rs
+++ worker/tests/event.rs
@@ -3,3 +3,9 @@ fn event_invalid_signatures() {
     let t = trybuild::TestCases::new();
     t.compile_fail("tests/ui/*.rs");
 }
+
+#[test]
+fn event_valid_signatures() {
+    let t = trybuild::TestCases::new();
+    t.pass("tests/pass/*.rs");
+}
