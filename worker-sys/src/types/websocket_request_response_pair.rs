dd8fbeab4dcc33f7c253e0c31e4291251f5d450f
Connor Hindley <conn.hindley@gmail.com>
2026-05-06 23:17:08 -0500

    wip: Support binary auto req/res.
    Currently broken without https://github.com/cloudflare/workerd/pull/6738

--- worker-sys/src/types/websocket_request_response_pair.rs
+++ worker-sys/src/types/websocket_request_response_pair.rs
@@ -1,3 +1,4 @@
+use js_sys::Uint8Array;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -9,9 +10,21 @@ extern "C" {
     #[wasm_bindgen(constructor, catch)]
     pub fn new(request: &str, response: &str) -> Result<WebSocketRequestResponsePair, JsValue>;
 
+    #[wasm_bindgen(constructor, catch, js_class = "WebSocketRequestResponsePair")]
+    pub fn new_bytes(
+        request: &Uint8Array,
+        response: &Uint8Array,
+    ) -> Result<WebSocketRequestResponsePair, JsValue>;
+
     #[wasm_bindgen(method, getter)]
     pub fn request(this: &WebSocketRequestResponsePair) -> String;
 
     #[wasm_bindgen(method, getter)]
     pub fn response(this: &WebSocketRequestResponsePair) -> String;
+
+    #[wasm_bindgen(method, getter, js_name = request)]
+    pub fn request_bytes(this: &WebSocketRequestResponsePair) -> Uint8Array;
+
+    #[wasm_bindgen(method, getter, js_name = response)]
+    pub fn response_bytes(this: &WebSocketRequestResponsePair) -> Uint8Array;
 }
