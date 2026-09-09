acde5011a263c08c7015ca803d5aca69d01aef50
Thomas Rubini <trubini@cloudflare.com>
2026-09-09 12:11:26 +0100

    add a Miniflare test for the connect handler

--- test/tests/socket.spec.ts
+++ test/tests/socket.spec.ts
@@ -1,4 +1,5 @@
 import {describe, test, expect} from "vitest";
+import { connect } from "node:net";
 import { mf, mfUrl } from "./mf-socket";
 
 describe("socket", () => {
@@ -9,5 +10,17 @@ describe("socket", () => {
   test("read", async () => {
     const resp = await mf.dispatchFetch(`${mfUrl}socket/read`);
     expect(resp.status).toBe(200);
+  });
+  test("inbound connection", async () => {
+    const response = await new Promise<Buffer>((resolve, reject) => {
+      const socket = connect(25001, "127.0.0.1", () => socket.write("ping"));
+      socket.once("data", (data) => {
+        socket.destroy();
+        resolve(data);
+      });
+      socket.once("error", reject);
+    });
+
+    expect(response.toString()).toBe("ping");
   });
 });
