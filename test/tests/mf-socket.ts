64f1369b1fda489ea084e1ad06722970e61ca13f
Thomas Rubini <trubini@cloudflare.com>
2026-09-25 02:09:20 +0100

    test: cover inbound UDP and TCP connections with protocol field condition

--- test/tests/mf-socket.ts
+++ test/tests/mf-socket.ts
@@ -10,27 +10,46 @@ export const server = createServer(function (socket) {
     });
 }).listen(8080);
 
+const manifest = {
+  mainModule: "build/index.js",
+  modules: {
+    "build/index.js": {
+      type: "esm" as const,
+      contents: readFileSync("./build/index.js", "utf8"),
+    },
+    "build/index_bg.wasm": {
+      type: "wasm" as const,
+      contents: new Uint8Array(readFileSync("./build/index_bg.wasm")),
+    },
+  },
+};
+
 export const mf = new Miniflare({
   workers: [{
     config: {
       name: "socket-test",
       compatibilityDate: "2024-12-05",
-      manifest: {
-        mainModule: "build/index.js",
-        modules: {
-          "build/index.js": {
-            type: "esm",
-            contents: readFileSync("./build/index.js", "utf8"),
-          },
-          "build/index_bg.wasm": {
-            type: "wasm",
-            contents: new Uint8Array(readFileSync("./build/index_bg.wasm")),
-          },
-        },
-      },
+      manifest,
       triggers: [{ type: "connect", protocol: "tcp", port: 0 }],
     },
   }],
 });
 
 export const mfUrl = await mf.ready;
+
+export const mfExperimental = new Miniflare({
+  workers: [{
+    config: {
+      name: "socket-test-experimental",
+      compatibilityDate: "2024-12-05",
+      compatibilityFlags: ["experimental"],
+      manifest,
+      triggers: [
+        { type: "connect", protocol: "tcp", port: 0 },
+        { type: "connect", protocol: "udp", port: 0 },
+      ],
+    },
+  }],
+});
+
+await mfExperimental.ready;
